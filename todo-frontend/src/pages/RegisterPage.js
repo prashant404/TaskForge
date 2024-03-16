485e6e5720d5ec87a11f3daa697596b1f12ca7ae
prashant404 <pkr214204@gmail.com>
2024-03-16 14:45:20 +0530

    Endpoints-Updated

--- todo-frontend/src/pages/RegisterPage.js
+++ todo-frontend/src/pages/RegisterPage.js
@@ -1,13 +1,13 @@
+// RegisterForm.js
 import React, { useState } from "react";
 import { Card, Form, Button, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const RegisterForm = () => {
   const [formData, setFormData] = useState({
     username: "",
-    email: "",
     password: "",
-    confirmPassword: "",
   });
   const [error, setError] = useState("");
 
@@ -19,32 +19,32 @@ const RegisterForm = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const { username, email, password, confirmPassword } = formData;
-    // Perform form validation here
-    if (!username || !email || !password || !confirmPassword) {
-      setError("All fields are required");
-      return;
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        {
+          username: formData.username,
+          password: formData.password,
+        }
+      );
+      console.log(response.data); // handle successful registration
+    } catch (error) {
+      console.error("Registration failed:", error.response.data.msg); // handle registration error
+      setError("Registration failed: " + error.response.data.msg); // Update error state to display error message
     }
-    if (password !== confirmPassword) {
-      setError("Passwords do not match");
-      return;
-    }
-    // If form is valid, submit the data
-    console.log("Form submitted:", formData);
   };
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-      <div className="w-50"> {/* Adjust width here */}
+      <div className="w-50">
         <Card className="p-4">
           <Card.Body>
             <h3 className="mb-4 logo-text text-center">TaskForge</h3>
             <h2 className="text-center mb-4">Register</h2>
             {error && <Alert variant="danger">{error}</Alert>}
             <Form onSubmit={handleSubmit}>
-              {/* Your registration form fields */}
               <Form.Group controlId="formBasicUsername">
                 <Form.Label>Username</Form.Label>
                 <Form.Control
@@ -55,16 +55,6 @@ const RegisterForm = () => {
                   onChange={handleChange}
                 />
               </Form.Group>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                />
-              </Form.Group>
               <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
@@ -72,16 +62,6 @@ const RegisterForm = () => {
                   placeholder="Password"
                   name="password"
                   value={formData.password}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicConfirmPassword">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Confirm Password"
-                  name="confirmPassword"
-                  value={formData.confirmPassword}
                   onChange={handleChange}
                 />
               </Form.Group>

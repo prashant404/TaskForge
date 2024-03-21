e3d3f89bc7df404958ff348dd6e35bcd03ffe75f
prashant404 <pkr214204@gmail.com>
2024-03-22 02:54:06 +0530

    server.js updated

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -8,7 +8,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin : ["https://task-forge-onmg.vercel.app/"],
+    methods : ["POST","PUT","GET"],
+    credentials:true
+  }
+));
 app.use(express.json());
 
 // MongoDB Connection

73b0d516807726d00ccd9b8c175533c1f44391fd
prashant404 <pkr214204@gmail.com>
2024-03-22 23:06:32 +0530

    cors updated

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -10,7 +10,13 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:[""],
+    methods:["GET","POST","PUT"],
+    credentials:true,
+  }
+));
 app.use(express.json());
 
 // MongoDB Connection

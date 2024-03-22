a7b351c3cd25294fada6cfc995bfdbf00bd2a923
prashant404 <pkr214204@gmail.com>
2024-03-22 22:53:02 +0530

    login endpoint

--- todo-frontend/src/pages/LoginPage.js
+++ todo-frontend/src/pages/LoginPage.js
@@ -12,7 +12,7 @@ const LoginPage = () => {
   const handleLogin = async (username, password) => {
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://task-forge-api.vercel.app/api/auth/login",
         {
           username,
           password,
